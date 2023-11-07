6c8b86091a2386f2ae2c6929cc8974507536ef7a
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2023-11-07 11:59:34 -0800

    Fetch Data

--- Assets/Js/Scripts.js
+++ Assets/Js/Scripts.js
@@ -7,75 +7,120 @@ document.addEventListener("DOMContentLoaded", function () {
   var xhttp = new XMLHttpRequest();
   //step 2:
    
-//   var url="https://myjson.online/app/records/ec391043-f7d5-4bb2-9611-210ee58f4729";
   var url="/Assets/Js/products.json";
   xhttp.open("GET", url);
   //step 3:
   xhttp.onreadystatechange = function () {
+
     if (this.readyState == 4 && this.status == 200) {
       var data =JSON.parse(xhttp.responseText);
-    //   createMainBox(data);
-    console.log(data);
-    }else{
-        alert('دیتا وجود ندارد ');
+      createMainBox(data);
     }
+  
   };
   xhttp.send();
 });
 
 
-// function createMainBox(data) {
-//   myData=data;
-//   var mainBox = document.querySelector("#container-filter");
+function createMainBox(data) {
 
-//   for (const item in myData.products) {
-//     var mainBoxElem = createGroupBox(
-//       myData.products[item].Name,
-//       myData.products[item].Code,
-//       myData.products[item].Price,
-//       myData.products[item].ImageUrl,
-//       myData.products[item].Satisfaction,
-//       myData.products[item].sale,
-//     );
-//     mainBox.appendChild(mainBoxElem);
-//   }
-}
+  myData=data;
 
-// function createGroupBox(Name,Code,ImageUrl,Price,Satisfaction,sale) {
+ var mainBox=document.querySelector("#container-filter");
 
-//   var boxItem = document.querySelector(".filter-item");
+  for (const item in myData.products) {
+    var mainBoxElem = createGroupBox(
+      myData.products[item].id,
+      myData.products[item].name,
+      myData.products[item].Price,
+      myData.products[item].ImageUrl,
+      myData.products[item].sale,
+      myData.products[item].Satisfaction
+    );
 
-//   boxItem.insertAdjacentElement("afterbegin", `
-//   <div class="filter-item all mens mt-2 mb-5 pt-3 pb-3 custom-shadow
-//                         d-flex flex-column justify-content-around align-items-center">
-//                            <div class="rating w-100 d-flex  justify-content-around align-items-center ">
-//                             <span><a href="" class="far fa-heart bg-purple"></a></span>
-//                             <span><a href="" class="far fa-eye bg-purple"></a></span>
-//                            </div>
-//                              <div class="filter-item_img mt-2">
-//                                 <img src="${ImageUrl}" alt="">
-//                                 <h6 class="mt-3 text-center"> ${Name}</h6>
-//                                 <span>${Code}</span>
-//                              </div>
-//                              <div class="stars mt-5">
-//                                 <a class="far fa-star"></a>
-//                                 <a class="far fa-star"></a>
-//                                 <a class="far fa-star"></a>
-//                                 <a class="far fa-star"></a>
-//                                 <a class="far fa-star-half-alt"></a>
-//                              </div>
-//                              <div class="product-price mt-4 d-flex flex-column justify-content-center align-items-center ">
-//                                 <span class="text-success ">${Price}<span  class="text-danger px-2">تومان</span></span>
-//                                 <div class="d-flex justify-content-between align-items-center mt-4">
-//                                     <span class="font-12 mx-2 color-greenYellow">${Satisfaction}<span class="text-dark px-2 ">رضایت</span> </span>
-//                                     <span class="font-12 color-gold">${sale}<span class="text-dark px-2 ">فروش</span> </span>
-//                                 </div>
-//                              </div>
-//                              <div class="add-cart p-2 mt-4 w-75  text-center bg-blueSky">
-//                                 <a href="" class="text-white" >افزودن به سبد</a>
-//                              </div>
-//                         </div>
-//   `)
+    mainBox.appendChild(mainBoxElem);
+  }
+
+
+function createGroupBox(id,name,Price,ImageUrl,Satisfaction,sale) {
   
-// //   return boxItem;
-// }
+  let productList=document.getElementById('product-list');
+
+  let filterItem=document.createElement('div')
+  filterItem.classList.add('filter-item')
+
+
+  let rating=document.createElement('div')
+  rating.classList.add('rating')
+
+  let spanHeart=document.createElement('span')
+  let heartLink=document.createElement('a')
+  heartLink.classList.add('fa-heart')
+  heartLink.innerHTML="hert"
+
+  let spanEye=document.createElement('span')
+  let eyeLink=document.createElement('a')
+  eyeLink.classList.add('fa-eye')
+
+  rating.append( spanHeart , spanEye)
+
+
+
+  let filterItemimg =document.createElement('div')//
+  filterItemimg.classList.add('filter-item_img')
+
+  let img=document.createElement('img')
+  img.setAttribute('src',ImageUrl)
+
+  filterItemimg.append(img)
+
+
+  let H6Name=document.createElement('h6');
+  H6Name.innerText= name;
+
+  let spanPrice=document.createElement('span');
+  let priceText=document.createTextNode("تومان");
+  spanPrice.innerText=Price;
+
+  spanPrice.append(priceText);
+
+
+  let SatisfactionDiv=document.createElement('div');//
+ SatisfactionDiv.classList.add('SatisfactionDiv');
+
+ let saleSpan=document.createElement('span');
+let saleText=document.createTextNode('فروش')
+ saleSpan.innerText=sale;
+  saleSpan.append(saleText)
+
+ let SatisfactionSpan=document.createElement('span');
+ SatisfactionSpan.classList.add('SatisfactionSpan')
+ let SatisfactionText=document.createTextNode("رضایت")
+ SatisfactionSpan.innerText=Satisfaction;
+
+ SatisfactionSpan.append(SatisfactionText)
+
+ SatisfactionDiv.append(saleSpan,SatisfactionSpan);
+
+ let addCart=document.createElement('div');
+ addCart.classList.add('add-cart');
+
+ let basketBtn=document.createElement('a');
+ basketBtn.setAttribute('href',"basket.html")
+ let basketBtnText=document.createTextNode('افزودن به سبد')
+ basketBtn.classList.add('basket-btn');
+
+ basketBtn.append(basketBtnText)
+
+ addCart.append(basketBtn);
+
+ filterItem.append(rating,filterItemimg,H6Name, spanPrice,SatisfactionDiv,addCart)
+
+productList.append(filterItem)
+
+console.log(productList);
+
+return productList;
+
+}
+}
